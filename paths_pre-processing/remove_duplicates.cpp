8188f4cf2c0e9d64f1ef87acd565ce1fc4525e9b
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-03-19 13:09:32 +0000

    Adding script to remove duplicates

---
+++ paths_pre-processing/remove_duplicates.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <string>
+#include <iterator>
+
+int main() {
+   std::ifstream inputFile("admin_path.txt");
+   
+   if (!inputFile) {
+      std::cerr << "Error opening file 'admin_path.txt'!" << std::endl;
+      return 1;
+   }
+
+   std::set<std::string> uniqueLines;
+   std::string line;
+
+   while (std::getline(inputFile, line)) {
+      uniqueLines.insert(line);
+   }
+   
+   inputFile.close();
+
+   std::ofstream outputFile("admin_path.txt");
+
+   if (!outputFile) {
+      std::cerr << "Error opening file 'admin_path.txt' for writing!" << std::endl;
+      return 1;
+   }
+
+   for (const auto& uniqueLine : uniqueLines) {
+      outputFile << uniqueLine << std::endl;
+   }
+
+   outputFile.close();
+   std::cout << "Duplicates removed and file updated successfully!" << std::endl;
+
+   return 0;
+}
